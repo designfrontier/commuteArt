df2995f6b1242f1ae62087737af276aef0ac6038
Daniel Sellers <daniel@designfrontier.net>
2013-03-06 09:17:06 -0700

    attempts

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
