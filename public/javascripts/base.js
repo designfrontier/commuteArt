1bf1686436f2195eac3de58511f4593250b7ca66
Daniel Sellers <daniel@designfrontier.net>
2013-03-11 15:43:42 -0600

    cleaned up the code and moved everything inside the commuteArt object

--- public/javascripts/base.js
+++ public/javascripts/base.js
@@ -12,37 +12,47 @@ var commuteArt = {
 	, hideIntro: function(){
 		var heading = $('.heading');
 
+		//is the header visible?
 		if(heading.attr('display') !== 'none'){
+			//yes hide it
 			heading.hide();
 		}
 	}
-};
 
-$(function(){
-	commuteArt.initializeCanvas();
-
-	//welcome to the world!
-	commuteArt.drawingSpace = document.getElementById('drawing').getContext('2d');
-
-	window.ondevicemotion = function(event) {
+	, drawMotion: function(event){
 		var ctx = commuteArt.drawingSpace
 			, xPosition = event.accelerationIncludingGravity.x * 42
 			, yPosition = event.accelerationIncludingGravity.y * 42
 			, zPosition = event.accelerationIncludingGravity.z * 10;
 
-			//default so we have some visible dot no matter what
-			if(zPosition < 2){
-				zPosition = 2;
-			}
+		//default so we have some visible dot no matter what
+		//	not having this can result in invisible dots
+		if(zPosition < 2){
+			zPosition = 2;
+		}
 
-			if(xPosition > 21 || yPosition > 21 || zPosition > 10){
-				//hide the header
-				commuteArt.hideIntro();
+		//check that we have crossed some threshold of amplitude
+		//	otherwise you end up with tons and tons of little dots
+		//	in the same place
+		if(xPosition > 21 || yPosition > 21 || zPosition > 10){
+			//hide the header
+			commuteArt.hideIntro();
 
-				ctx.beginPath();
-				ctx.fillStyle = 'rgba(0,0,0,0.3)';
-				ctx.arc(xPosition, yPosition, zPosition, 0, Math.PI*2, true);
-				ctx.fill();
-			}
+			ctx.beginPath();
+			ctx.fillStyle = 'rgba(0,0,0,0.3)';
+			ctx.arc(xPosition, yPosition, zPosition, 0, Math.PI*2, true);
+			ctx.fill();
+		}
 	}
+};
+
+$(function(){
+	//setup the space for the drawing to happen
+	commuteArt.initializeCanvas();
+
+	//welcome to the world!
+	commuteArt.drawingSpace = document.getElementById('drawing').getContext('2d');
+
+	//onDeviceMotion do some drawing
+	window.ondevicemotion = commuteArt.drawMotion;
 });
